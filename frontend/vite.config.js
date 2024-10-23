9b24001e77b7c74cd5e9fc028f6c79c419fcc849
Saurabh Singh Kunwar <saurabhsinghkunwar22200@gmail.com>
2024-10-23 20:13:54 +0530

    cors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api" : {
-        target: 'https://blogger-app-rouge.vercel.app',
+        target: 'https://blogger-app-duq6.onrender.com',
         changeOrigin : true,
       }
     }
